f3456462fff410dc87fbd5b6685ad6d52289321a
Chankyu Kim <chkiya@gmail.com>
2022-12-13 16:33:27 +0900

    test12

--- localCreationTest/hello_world/app.py
+++ localCreationTest/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "test Local 2 aa1122aaaaaa",
+            "message": "test Local 2",
             # "location": ip.text.replace("\n", "")
         }),
     }
